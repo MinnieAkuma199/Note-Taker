739283c3b4d4ec1b0993522cbe3d0b5becd97870
Demon <kristyndcb@gmail.com>
2022-06-07 15:21:35 -0600

    get and post working correctly

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const notes = require("./db/db.json");
-const fs = require("fs"); //do i need fs here?
+const fs = require("fs"); //need fs to readfile db.json
+
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = 3001;
@@ -11,16 +13,73 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//read-- creating the homepage
+//linking to the index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
-//get notes
-app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html")); //do i need this here?
-  res.status(200).json(notes);
+//linking to notes.html and displaying
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+//connecting /notes to db.json
+//can not have 2 get routes with the very same path
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      res.status(200).json(parsedData);
+    }
+  });
+});
+
+//need post so when user puts in note it goes to notes (db.json)
+app.post("/api/notes", (req, res) => {
+  console.log(req.body); //this helps find that title and text are keys already passed in from front end
+  const { title, text } = req.body;
+  if (title && text) {
+    req.body.id = uuidv4();
+    readAndAppend(req.body, "./db/db.json");
+
+    res.status(200).json(req.body);
+  } else {
+    res.status(400).send("Error in user request");
+  }
+});
+
+//SHould i close this function here? and make the others global??
+// Convert the data to a string so we can save it
+// const notesString = JSON.stringify(newNote);
+// Write the string to a file
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const writeToFile = (destination, content) => {
+  console.log(content, destination);
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+};
+// const response = {
+//   status: "success",
+//   body: newNote,
+// };
+
+// console.log(response);
+// res.status(201).json(response);
+//  else {
+//     res.status(500).json("Error in posting review")
+//   };
 
 //listen
 app.listen(PORT, () =>
